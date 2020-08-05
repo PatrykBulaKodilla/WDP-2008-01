ae4a9d7d2208b80b788669c838f28bca437849c1
Karol <karolsobolewski92@gmail.com>
2020-08-05 19:46:30 +0200

    Add Hover effect for feature list && Change FeatureBox components to links

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -44,6 +44,7 @@ const FeatureBoxes = () => (
     </div>
   </div>
 );
+
 
 FeatureBoxes.propTypes = {
   children: PropTypes.node,
