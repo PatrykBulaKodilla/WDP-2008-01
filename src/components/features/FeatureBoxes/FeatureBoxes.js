56b99d3631ed8e89b3e3bfcd98504ebb2346eb0f
Karol <karolsobolewski92@gmail.com>
2020-08-05 19:46:30 +0200

    Add Hover effect for feature list && Change FeatureBox components to links

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import {
   faTruck,
@@ -11,38 +12,52 @@ import {
 import styles from './FeatureBoxes.module.scss';
 import FeatureBox from '../../common/FeatureBox/FeatureBox';
 
-const FeatureBoxes = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-          <FeatureBox icon={faTruck} active>
-            <h5>Free shipping</h5>
-            <p>All orders</p>
-          </FeatureBox>
-        </div>
-        <div className='col'>
-          <FeatureBox icon={faHeadphones}>
-            <h5>24/7 customer</h5>
-            <p>support</p>
-          </FeatureBox>
-        </div>
-        <div className='col'>
-          <FeatureBox icon={faReplyAll}>
-            <h5>Money back</h5>
-            <p>guarantee</p>
-          </FeatureBox>
-        </div>
-        <div className='col'>
-          <FeatureBox icon={faBullhorn}>
-            <h5>Member discount</h5>
-            <p>First order</p>
-          </FeatureBox>
+const FeatureBoxes = () => {
+  //const [active] = React.useState(true);
+  //const [hovered, setHovered] = useState(false);
+  //const toggleHover = () => setHovered(!hovered);
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <FeatureBox
+              icon={faTruck}
+              /*active={hovered ? {active} : ''}
+              onMouseEnter={toggleHover}
+              onMouseLeave={toggleHover}*/
+              component={Link}
+              exact
+              to={``}
+            >
+              <h5>Free shipping</h5>
+              <p>All orders</p>
+            </FeatureBox>
+          </div>
+          <div className='col'>
+            <FeatureBox icon={faHeadphones} component={Link} exact to={``}>
+              <h5>24/7 customer</h5>
+              <p>support</p>
+            </FeatureBox>
+          </div>
+          <div className='col'>
+            <FeatureBox icon={faReplyAll} component={Link} exact to={``}>
+              <h5>Money back</h5>
+              <p>guarantee</p>
+            </FeatureBox>
+          </div>
+          <div className='col'>
+            <FeatureBox icon={faBullhorn} component={Link} exact to={``}>
+              <h5>Member discount</h5>
+              <p>First order</p>
+            </FeatureBox>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 FeatureBoxes.propTypes = {
   children: PropTypes.node,
