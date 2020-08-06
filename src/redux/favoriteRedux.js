9f6ddb1eda51fca4c42ea30354a8ed9f10d3b676
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-06 16:18:52 +0200

    Create new aztion

--- src/redux/favoriteRedux.js
+++ src/redux/favoriteRedux.js
@@ -6,12 +6,18 @@ const reducerName = 'cart';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 /* action types */
+const SET_FAVORITE_PRODUCT = createActionName('SET_FAVORITE_PRODUCT');
 
 /* action creators */
+export const setFavoriteProduct = payload => ({ payload, type: SET_FAVORITE_PRODUCT });
 
 /* reducer */
 export default function reducer(statePart = [], action = []) {
   switch (action.type) {
+    case SET_FAVORITE_PRODUCT: {
+      console.log(statePart);
+      return statePart;
+    }
     default:
       return statePart;
   }
