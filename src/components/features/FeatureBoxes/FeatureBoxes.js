13b0f13ff5d754b61e94fa71be847f514c4ef6a4
Karol <karolsobolewski92@gmail.com>
2020-08-06 20:03:04 +0200

    Delete unused optios and comments

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -12,52 +12,38 @@ import {
 import styles from './FeatureBoxes.module.scss';
 import FeatureBox from '../../common/FeatureBox/FeatureBox';
 
-const FeatureBoxes = () => {
-  //const [active] = React.useState(true);
-  //const [hovered, setHovered] = useState(false);
-  //const toggleHover = () => setHovered(!hovered);
-
-  return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            <FeatureBox
-              icon={faTruck}
-              /*active={hovered ? {active} : ''}
-              onMouseEnter={toggleHover}
-              onMouseLeave={toggleHover}*/
-              component={Link}
-              exact
-              to={``}
-            >
-              <h5>Free shipping</h5>
-              <p>All orders</p>
-            </FeatureBox>
-          </div>
-          <div className='col'>
-            <FeatureBox icon={faHeadphones} component={Link} exact to={``}>
-              <h5>24/7 customer</h5>
-              <p>support</p>
-            </FeatureBox>
-          </div>
-          <div className='col'>
-            <FeatureBox icon={faReplyAll} component={Link} exact to={``}>
-              <h5>Money back</h5>
-              <p>guarantee</p>
-            </FeatureBox>
-          </div>
-          <div className='col'>
-            <FeatureBox icon={faBullhorn} component={Link} exact to={``}>
-              <h5>Member discount</h5>
-              <p>First order</p>
-            </FeatureBox>
-          </div>
+const FeatureBoxes = () => (
+  <div className={styles.root}>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <FeatureBox icon={faTruck} component={Link} exact to={``}>
+            <h5>Free shipping</h5>
+            <p>All orders</p>
+          </FeatureBox>
+        </div>
+        <div className='col'>
+          <FeatureBox icon={faHeadphones} component={Link} exact to={``}>
+            <h5>24/7 customer</h5>
+            <p>support</p>
+          </FeatureBox>
+        </div>
+        <div className='col'>
+          <FeatureBox icon={faReplyAll} component={Link} exact to={``}>
+            <h5>Money back</h5>
+            <p>guarantee</p>
+          </FeatureBox>
+        </div>
+        <div className='col'>
+          <FeatureBox icon={faBullhorn} component={Link} exact to={``}>
+            <h5>Member discount</h5>
+            <p>First order</p>
+          </FeatureBox>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 FeatureBoxes.propTypes = {
   children: PropTypes.node,
