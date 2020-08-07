758adcd69e3485a3d27e0bccea66c936acb3841c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-07 13:27:37 +0200

    Add methods for handling onTouch events

--- src/components/features/Swipeable/Swipeable.js
+++ src/components/features/Swipeable/Swipeable.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './Swipeable.module.scss';
-
 const Swipeable = props => {
-  return <div className={styles.root}>{props.children}</div>;
+  let positionStart = 0;
+  let positionEnd = 0;
+  let swipeDirection;
+
+  const handleTouchStart = event => {
+    positionStart = event.touches[0].clientX;
+  };
+
+  const handleTouchMove = event => {
+    positionEnd = event.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    if (positionEnd <= positionStart * 0.9) {
+      swipeDirection = 'right';
+      console.log(swipeDirection);
+    } else if (positionEnd >= positionStart * 1.1) {
+      swipeDirection = 'left';
+      console.log(swipeDirection);
+    }
+  };
+
+  return (
+    <div
+      onTouchStart={event => handleTouchStart(event)}
+      onTouchMove={event => handleTouchMove(event)}
+      onTouchEnd={() => handleTouchEnd()}
+    >
+      {console.log(props)}
+      {props.children}
+    </div>
+  );
 };
 
 Swipeable.propTypes = {
