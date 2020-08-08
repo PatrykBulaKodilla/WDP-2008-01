26b95d0f2efb7a200b997ce3c479a09300cd8861
Michał Kopacz <kopaczmichal093@gmail.com>
2020-08-08 13:11:33 +0200

    Add RWD styles for FooterMenu

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = ({ children }) => (
     <div className={styles.footerMenu}>
       <div className='container'>
         <div className='row'>
-          <div className='col'>
+          <div className='col-12 col-sm-6 col-md-3'>
             <div className={styles.menuWrapper}>
               <h6>Information</h6>
               <ul>
@@ -36,7 +36,7 @@ const Footer = ({ children }) => (
               </ul>
             </div>
           </div>
-          <div className='col'>
+          <div className='col-12 col-sm-6 col-md-3'>
             <div className={styles.menuWrapper}>
               <h6>My account</h6>
               <ul>
@@ -55,7 +55,7 @@ const Footer = ({ children }) => (
               </ul>
             </div>
           </div>
-          <div className='col'>
+          <div className='col-12 col-sm-6 col-md-3'>
             <div className={styles.menuWrapper}>
               <h6>Information</h6>
               <ul>
@@ -74,7 +74,7 @@ const Footer = ({ children }) => (
               </ul>
             </div>
           </div>
-          <div className='col'>
+          <div className='col-12 col-sm-6 col-md-3'>
             <div className={styles.menuWrapper}>
               <h6>Orders</h6>
               <ul>
@@ -92,7 +92,9 @@ const Footer = ({ children }) => (
                 </li>
               </ul>
             </div>
-            <img src='./images/cards.png' alt='Supported credit cards' />
+            <div className={styles.menuCards}>
+              <img src='./images/cards.png' alt='Supported credit cards' />
+            </div>
           </div>
         </div>
       </div>
