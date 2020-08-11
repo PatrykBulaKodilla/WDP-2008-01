07a5efa096750b9b4a3dd1c721a29a0430aae48a
Karol <karolsobolewski92@gmail.com>
2020-08-11 18:10:04 +0200

    Add swipeable component to slider

--- src/components/features/Slider/Slider.js
+++ src/components/features/Slider/Slider.js
@@ -3,6 +3,7 @@ import styles from './Slider.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
+import Swipeable from '../Swipeable/Swipeable';
 
 class Slider extends React.Component {
   handlePageChange(newPage) {
@@ -13,34 +14,51 @@ class Slider extends React.Component {
     this.setState({ activeCategory: newCategory });
   }
 
+  handleLeftAction(pagesCount) {
+    if (this.state.activePage < pagesCount - 1) {
+      this.setState({ activePage: this.state.activePage + 1 });
+    }
+  }
+
+  handleRightAction() {
+    if (this.state.activePage > 0) {
+      this.setState({ activePage: this.state.activePage - 1 });
+    }
+  }
+
   render() {
     return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.photo}>
-            <div className={styles.panel}>
-              <div className={styles.descriptionTitle}>
-                <p>indoor</p>
-                <p>furniture</p>
+      <Swipeable
+        leftAction={() => this.handleLeftAction()}
+        rightAction={() => this.handleRightAction()}
+      >
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={styles.photo}>
+              <div className={styles.panel}>
+                <div className={styles.descriptionTitle}>
+                  <p>indoor</p>
+                  <p>furniture</p>
+                </div>
+                <div className={styles.descriptionSubitle}>
+                  <p>Save up to 50% of all furniture</p>
+                </div>
+                <Button variant='small' className={styles.buttonDescription}>
+                  SHOP NOW
+                </Button>
               </div>
-              <div className={styles.descriptionSubitle}>
-                <p>Save up to 50% of all furniture</p>
+              <div className='row'>
+                <Button variant='small' className={styles.button}>
+                  <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+                </Button>
+                <Button variant='small' className={styles.button}>
+                  <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                </Button>
               </div>
-              <Button variant='small' className={styles.buttonDescription}>
-                SHOP NOW
-              </Button>
-            </div>
-            <div className='row'>
-              <Button variant='small' className={styles.button}>
-                <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
-              </Button>
-              <Button variant='small' className={styles.button}>
-                <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-              </Button>
             </div>
           </div>
         </div>
-      </div>
+      </Swipeable>
     );
   }
 }
