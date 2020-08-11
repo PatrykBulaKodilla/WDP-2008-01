940dd27f8b2aab76a1f3079f6c33796fe4a55230
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-11 16:51:52 +0200

    Add eventListener to window

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -4,13 +4,23 @@ import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children }) => {
+  window.addEventListener('resize', e => {
+    console.log(window.innerWidth);
+  });
+
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+MainLayout.state = {
+  test: 0,
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
