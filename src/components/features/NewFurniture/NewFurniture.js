6d1a300c1e9b658b5395c637a9fc252c6617cdf1
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-12 21:50:33 +0200

    Modify style for NewFurniture

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -100,7 +100,7 @@ class NewFurniture extends React.Component {
         <div className={styles.root}>
           <div className='container'>
             <div className={styles.panelBar}>
-              <div className='row no-gutters align-items-end'>
+              <div className='row justify-content-center no-gutters align-items-end'>
                 <div className={'col-auto ' + styles.heading}>
                   <h3>New furniture</h3>
                 </div>
