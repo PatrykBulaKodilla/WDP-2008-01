c58bd5a0e4c56f972f06c6326987fa6812c1f54e
Karol <karolsobolewski92@gmail.com>
2020-08-13 21:18:12 +0200

    Add columns for Shop page

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,7 +2,19 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => {
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className={`col col-sm-12 col-lg-9 order-3 order-md-1`}>LEFT</div>
+          <div className='w-100 d-block d-md-none order-2'></div>
+          <div className={`col col-sm-12 col-lg-3 order-1 order-md-3`}>RIGHT</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // ProductList.propTypes = {};
 
