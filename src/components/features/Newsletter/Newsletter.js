418e05f463ffff2bf7c82554365bb72563984506
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-14 16:31:44 +0200

    Create Newsletter component with basic styles

---
+++ src/components/features/Newsletter/Newsletter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Newsletter.module.scss';
+
+const Newsletter = () => (
+  <div className={styles.root}>
+    <h1>Newsletter</h1>
+  </div>
+);
+
+export default Newsletter;
