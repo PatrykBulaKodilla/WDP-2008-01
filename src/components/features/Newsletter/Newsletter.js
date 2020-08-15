92feebcdaa90e7fdc298d199288f9c81bacc10d0
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-15 13:29:12 +0200

    Create input section

--- src/components/features/Newsletter/Newsletter.js
+++ src/components/features/Newsletter/Newsletter.js
@@ -1,9 +1,19 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Newsletter.module.scss';
 
 const Newsletter = () => (
   <div className={styles.root}>
-    <h1>Newsletter</h1>
+    <div className='container'>
+      <h2 className={styles.title}>Newsletter:</h2>
+      <div className={styles.newsInput}>
+        <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+        <input type='email' placeholder='Enter your email address' required />
+      </div>
+    </div>
   </div>
 );
 
