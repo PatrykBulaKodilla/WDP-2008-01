98bd36fbf5e01f4add4c42c6652958a0302cbbaa
Michał Kopacz <kopaczmichal093@gmail.com>
2020-08-18 18:42:40 +0200

    Add left side of productPage

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,12 +1,54 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class ProductPage extends React.Component {
   render() {
     return (
       <div className={styles.root}>
-        <div className='text-center'>This is ProductPage</div>
+        <div className='container'>
+          <div className={styles.frame}>
+            <div className='row'>
+              <div className='col-5'>
+                <div className='row'>
+                  <div className='col-12'>
+                    <div className={styles.bigPic}>
+                      <img />
+                    </div>
+                  </div>
+                  <div className='col-12'>
+                    <div className={styles.slide}>
+                      <button className={styles.buttonLeft}>
+                        <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+                      </button>
+                      <div className={styles.slideGallery}>
+                        <img className={styles.imgLeft} />
+                        <img />
+                        <img />
+                        <img className={styles.imgRight} />
+                      </div>
+                      <button className={styles.buttonRight}>
+                        <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-7'>
+                <div className={styles.rightButtons}>
+                  <button>
+                    <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+                  </button>
+                  <button>
+                    <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
