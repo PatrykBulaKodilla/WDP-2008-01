de382832ad329e6453d4877e648ca93d9a011bce
Michał Kopacz <kopaczmichal093@gmail.com>
2020-08-18 15:06:43 +0200

    Create class component ProductPage

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,7 +2,15 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+class ProductPage extends React.Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className='text-center'>This is ProductPage</div>
+      </div>
+    );
+  }
+}
 
 // ProductPage.propTypes = {};
 
