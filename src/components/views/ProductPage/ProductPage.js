8c63ac5139c10629bca6c47b66cc4a689dd6a5b5
Michał Kopacz <kopaczmichal093@gmail.com>
2020-08-19 16:35:24 +0200

    Finish right side of productPage

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,8 +1,27 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
-import { faArrowLeft, faArrowRight, faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import {
+  faArrowLeft,
+  faArrowRight,
+  faStar,
+  faExchangeAlt,
+  faShoppingBasket,
+  faMinus,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faStar as farStar,
+  faHeart,
+  faEnvelope,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faFacebookF,
+  faGooglePlusG,
+  faTwitter,
+  faPinterestP,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class ProductPage extends React.Component {
@@ -73,11 +92,113 @@ class ProductPage extends React.Component {
                       </div>
                     </div>
                   </div>
-                  <div className='col-12'></div>
-                  <div className='col-12'></div>
-                  <div className='col-12'></div>
-                  <div className='col-12'></div>
-                  <div className='col-12'></div>
+                  <div className='col-12'>
+                    <div className={styles.lvl + ' ' + styles.shorterLvl}>
+                      <div className={styles.lvlTwo}>
+                        <div className={styles.oldPrice}>$350.00</div>
+                        <div className={styles.newPrice}>$250.00</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-12'>
+                    <div className={styles.lvl}>
+                      <div className='row'>
+                        <div className='col-12'>
+                          <div className={styles.lvlThree}>
+                            <button>
+                              <FontAwesomeIcon
+                                icon={faShoppingBasket}
+                              ></FontAwesomeIcon>
+                              <strong>ADD TO CART</strong>
+                            </button>
+                            <button>
+                              <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+                            </button>
+                            <button>
+                              <FontAwesomeIcon icon={faExchangeAlt}></FontAwesomeIcon>
+                            </button>
+                            <button>
+                              <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                            </button>
+                          </div>
+                        </div>
+                        <div className='col-12'>
+                          <div className={styles.lvlThree}>
+                            <div className={styles.quant}>Quantity: </div>
+                            <button className={styles.number}>2</button>
+                            <button>
+                              <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                            </button>
+                            <button>
+                              <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-12'>
+                    <div className={styles.lvl}>
+                      <div className='row'>
+                        <div className='col-12'>
+                          <div className={styles.lvlFour}>
+                            <span className={styles.overview}>Quick Overview</span>
+                          </div>
+                        </div>
+                        <div className='col-12'>
+                          <div className={styles.lvlFour2}>
+                            There are many variations of passages of Lorem Ipsum
+                            available, but the majority have suffered alteration in some
+                            form, by injected humour.
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-12'>
+                    <div className={styles.lvl}>
+                      <div className='row'>
+                        <div className='col-12'>
+                          <div className={styles.lvlFive}>
+                            <div>Availability:</div>
+                            <span>In Stock</span>
+                          </div>
+                        </div>
+                        <div className='col-12'>
+                          <div className={styles.lvlFive}>
+                            <div>Category:</div>
+                            <span>Furniture</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-12'>
+                    <div className={styles.lvl + ' ' + styles.lastLvl}>
+                      <div className={styles.lvlSix}>
+                        <button className={styles.facebook}>
+                          <FontAwesomeIcon icon={faFacebookF} />
+                          <span>Share</span>
+                        </button>
+                        <button className={styles.google}>
+                          <FontAwesomeIcon icon={faGooglePlusG} />
+                          <span>Google+</span>
+                        </button>
+                        <button className={styles.twitter}>
+                          <FontAwesomeIcon icon={faTwitter} />
+                          <span>Tweet</span>
+                        </button>
+                        <button className={styles.pinterest}>
+                          <FontAwesomeIcon icon={faPinterestP} />
+                          <span>Pinterest</span>
+                        </button>
+                        <button className={styles.linkedin}>
+                          <FontAwesomeIcon icon={faLinkedinIn} />
+                          <span>Linkedin</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
