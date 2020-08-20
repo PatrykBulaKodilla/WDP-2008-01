f0a7914d7b0fcdb255be99a87541e4d694f6e784
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-20 20:03:25 +0200

    Create SmallFurniture component

---
+++ src/components/features/SmallFurniture/SmallFurniture.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './SmallFurniture.module.scss';
+
+class SmallFurniture extends React.Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        <h2 className={styles.subTitle}>Small Furniture</h2>
+      </div>
+    );
+  }
+}
