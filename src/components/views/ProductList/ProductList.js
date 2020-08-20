f48ec1637124277eba48fe0ab04e00200cf0ae53
Kacper Wiśniewski <kaka1341@gmail.com>
2020-08-20 23:19:22 +0200

    Add shopList props to ProductList component & Change styles for buttons in ProductBox

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,16 +1,29 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import Filter from '../../common/Filter/Filter';
 
 import FilterByCategories from '../../common/FilterByCategories/FilterByCategories';
+import ProductBox from '../../common/ProductBox/ProductBoxContainer';
 
-const ProductList = () => {
+const ProductList = ({ shopList }) => {
+  let activePage = 0;
+  let ProductsOnPage = 20;
   return (
     <div className={styles.root}>
       <div className='container'>
         <div className='row'>
-          <div className={`col col-sm-12 col-lg-9 order-3 order-md-1`}>LEFT</div>
+          <div className={`col col-sm-12 col-lg-9 order-3 order-md-1`}>
+            <div className='row'>
+              {shopList
+                .slice(activePage * ProductsOnPage, (activePage + 1) * ProductsOnPage)
+                .map(item => (
+                  <div key={item.id} className='col-lg-3 col-md-4 col-6'>
+                    <ProductBox {...item} />
+                  </div>
+                ))}
+            </div>
+          </div>
           <div className='w-100 d-block d-md-none order-2'></div>
           <div className={`col col-sm-12 col-lg-3 order-1 order-md-3`}>
             <Filter />
@@ -24,6 +37,8 @@ const ProductList = () => {
   );
 };
 
-// ProductList.propTypes = {};
+ProductList.propTypes = {
+  shopList: PropTypes.array,
+};
 
 export default ProductList;
