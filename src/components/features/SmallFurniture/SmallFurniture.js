be749ab8231c23cd73f30385c4e7bb3bc6542ed7
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-20 21:08:55 +0200

    Create simple structure for SmallFurniture component

--- src/components/features/SmallFurniture/SmallFurniture.js
+++ src/components/features/SmallFurniture/SmallFurniture.js
@@ -7,6 +7,28 @@ class SmallFurniture extends React.Component {
     return (
       <div className={styles.root}>
         <h2 className={styles.title}>Small Furniture</h2>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <div className='row'>
+                <div className={`col-6 ${styles.image}`}></div>
+                <div className='col-6'></div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className={`col-6 ${styles.image}`}></div>
+                <div className='col-6'></div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className={`col-6 ${styles.image}`}></div>
+                <div className='col-6'></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
