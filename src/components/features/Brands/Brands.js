c080e75e8e0fada69d75394a313523163e7a95b8
Michał Kopacz <kopaczmichal093@gmail.com>
2020-08-21 21:01:53 +0200

    Add Swipeable to brands

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -3,6 +3,7 @@ import styles from './Brands.module.scss';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
+import Swipeable from '../Swipeable/Swipeable';
 
 class Brands extends React.Component {
   state = {
@@ -28,7 +29,7 @@ class Brands extends React.Component {
       this.addClass(this.state.brandsListRef, styles.slideRight);
 
       setTimeout(() => {
-        this.setState({ activePage: this.state.activePage + 1 });
+        this.handlePageChange(this.state.activePage + 1);
         this.addClass(this.state.brandsListRef, styles.fadeInRight);
         this.removeClass(this.state.brandsListRef, styles.slideRight);
       }, 1000);
@@ -42,11 +43,27 @@ class Brands extends React.Component {
       this.addClass(this.state.brandsListRef, styles.slideLeft);
 
       setTimeout(() => {
-        this.setState({ activePage: this.state.activePage - 1 });
+        this.handlePageChange(this.state.activePage - 1);
         this.addClass(this.state.brandsListRef, styles.fadeInLeft);
         this.removeClass(this.state.brandsListRef, styles.slideLeft);
       }, 1000);
     }
+  }
+
+  handleLeftAction(pagesCount) {
+    if (this.state.activePage < pagesCount - 1) {
+      this.setState({ activePage: this.state.activePage + 1 });
+    }
+  }
+
+  handleRightAction() {
+    if (this.state.activePage > 0) {
+      this.setState({ activePage: this.state.activePage - 1 });
+    }
+  }
+
+  handlePageChange(newPage) {
+    this.setState({ activePage: newPage });
   }
 
   render() {
@@ -54,44 +71,48 @@ class Brands extends React.Component {
     const { activePage } = this.state;
 
     const thisBrands = this;
-
     const brandsCount = Math.ceil(brands.length / brandsOnPage);
+    console.log('aaa', this.state.brandsListRef);
+    console.log('active', this.state.activePage);
 
     window.addEventListener('resize', () => {
-      if (brandsCount < activePage)
-        thisBrands.setState({ activePage: brandsCount - 1 });
+      if (brandsCount < activePage) thisBrands.handlePageChange(brandsCount - 1);
     });
-    console.log(window.innerWidth);
 
     return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.border}>
-            <button className={styles.button} onClick={() => this.previousPage()}>
-              <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
-            </button>
-            <div className={styles.sliderContainer}>
-              <div ref={this.state.brandsListRef}>
-                <ul className={styles.brands}>
-                  {brands
-                    .slice(activePage * brandsOnPage, (activePage + 1) * brandsOnPage)
-                    .map(brand => (
-                      <li key={brand.id}>
-                        <img src={brand.image} alt={brand.alt} />
-                      </li>
-                    ))}
-                </ul>
+      <Swipeable
+        leftAction={() => this.handleLeftAction(brandsCount)}
+        rightAction={() => this.handleRightAction()}
+      >
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={styles.border}>
+              <button className={styles.button} onClick={() => this.previousPage()}>
+                <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+              </button>
+              <div className={styles.sliderContainer}>
+                <div ref={this.state.brandsListRef}>
+                  <ul className={styles.brands}>
+                    {brands
+                      .slice(activePage * brandsOnPage, (activePage + 1) * brandsOnPage)
+                      .map(brand => (
+                        <li key={brand.id}>
+                          <img src={brand.image} alt={brand.alt} />
+                        </li>
+                      ))}
+                  </ul>
+                </div>
               </div>
+              <button
+                className={`${styles.button} ${styles.button2}`}
+                onClick={() => this.nextPage()}
+              >
+                <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+              </button>
             </div>
-            <button
-              className={`${styles.button} ${styles.button2}`}
-              onClick={() => this.nextPage()}
-            >
-              <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-            </button>
           </div>
         </div>
-      </div>
+      </Swipeable>
     );
   }
 }
